f62413119265c9a8d819e3ab176734235781c20f
korut94 <andreamnt94@gmail.com>
2014-12-15 20:46:53 +0100

    Primo abbozzo di Utente

---
+++ LinQedIn/utente.cpp
@@ -0,0 +1,5 @@
+#include "utente.h"
+
+Utente::Utente()
+{
+}
---
+++ LinQedIn/utente.h
@@ -0,0 +1,15 @@
+#ifndef UTENTE_H
+#define UTENTE_H
+
+#include "info.h"
+
+class Utente
+{
+    private:
+        Info info;
+
+    public:
+        Utente();
+};
+
+#endif // UTENTE_H
