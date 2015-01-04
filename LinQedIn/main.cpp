e640389240d89d025d0af2f62d45102894609845
korut94 <andreamnt94@gmail.com>
2015-01-04 17:48:22 +0100

    Test

--- LinQedIn/main.cpp
+++ LinQedIn/main.cpp
@@ -3,7 +3,15 @@
 
 #include <iostream>
 
+#include "hashgrouputente.h"
+#include "hashlistutente.h"
 #include "utente.h"
+#include "smartptr_utente.h"
+#include "sortlist.h"
+#include "sortgrouputente.h"
+
+typedef SortList<SortGroupUtente::AlfabeticoCre,smartptr_utente>
+        SortListName;
 
 int main(int argc, char *argv[])
 {
@@ -12,18 +20,44 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
+    SortListName list;
+
     Info info1( "Andrea",
                 "Mantovani",
                 "3406936174",
                 "17/09/1994");
+    Info info2( "Zottino",
+                "Mantovani",
+                "3406936174",
+                "17/09/1994");
+    Info info3( "Fiestrella",
+                "Mantovani",
+                "3406936174",
+                "17/09/1994");
 
-    sptr_utente utente = new Utente( "korut94", "36633663", info1 );
-    sptr_utente utente1 = new Utente( "Ciccio", "Nano", info1 );
+    smartptr_utente utenti[3];
 
-    utente = utente1;
+    utenti[0] = new Utente( "korut94", "36633663", info1 );
+    utenti[1] = new Utente( "bilbo94", "36633663", info2 );
+    utenti[2] = new Utente( "gianni94", "36633663", info3 );
 
-    Info & info = utente->getInfo();
-    std::cout << info.getNome() << std::endl;
+
+    for( int i = 0; i < 3; i++ ) list.insert( utenti[i] );
+    for( SortListName::const_iterator itr = list.constBegin();
+         itr != list.end();
+         itr++ )
+    {
+        std::cout << **itr << std::endl;
+    }
+
+    HashGroupUtente::AlfabeticoDecUpCase c;
+
+    std::cout << c( "Babbo" ) << std::endl;
+
+    HashListUtente<HashGroupUtente::AlfabeticoDecUpCase,
+                   SortGroupUtente::AlfabeticoDec> database;
+
+
 
     return a.exec();
 }
