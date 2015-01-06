8f172421516b44f470b86c5a224fc88820f13515
korut94 <andreamnt94@gmail.com>
2015-01-06 19:58:58 +0100

    Aggiunto funtore UgualeAdUtente in SearchGroupUtente

--- LinQedIn/searchgrouputente.h
+++ LinQedIn/searchgrouputente.h
@@ -1,10 +1,21 @@
 #ifndef SEARCHGROUPUTENTE_H
 #define SEARCHGROUPUTENTE_H
 
-class SearchGroupUtente
+#include "smartptr_utente.h"
+#include "utente.h"
+
+namespace SearchGroupUtente
 {
-public:
-    SearchGroupUtente();
-};
+    class UgualeAdUtente
+    {
+        private:
+            const Utente & user;
+
+        public:
+            UgualeAdUtente( const Utente & );
+
+            bool operator()( const smartptr_utente & ) const;
+    };
+}
 
 #endif // SEARCHGROUPUTENTE_H
