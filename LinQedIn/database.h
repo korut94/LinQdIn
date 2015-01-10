7627ec95612ae9262783c402cede64a48a628530
korut94 <andreamnt94@gmail.com>
2015-01-10 16:52:19 +0100

    Inserite i metodi principali

--- LinQedIn/database.h
+++ LinQedIn/database.h
@@ -1,10 +1,35 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <iostream>
+
+#include "hashgrouputente.h"
+#include "hashlistutente.h"
+#include "searchgrouputente.h"
+#include "sortgrouputente.h"
+#include "utente.h"
+
 class Database
 {
-public:
-    Database();
+    private:
+        bool modified;
+
+        HashListUtente<HashGroupUtente::NameCrescente,
+                       SortGroupUtente::NameCrescente> entryName;
+        HashListUtente<HashGroupUtente::UsernameCrescente,
+                       SortGroupUtente::UsernameCrescente> entryUsername;
+
+    public:
+        Database();
+
+        bool isPresent( const smartptr_utente & ) const;
+
+        smartptr_utente getUser( const QString & ) const;
+
+        void insert( const smartptr_utente & );
+        void linkUser( const smartptr_utente &, const smartptr_utente & );
+        void modify( const smartptr_utente &, const Info & );
+        void remove( const smartptr_utente & );
 };
 
 #endif // DATABASE_H
