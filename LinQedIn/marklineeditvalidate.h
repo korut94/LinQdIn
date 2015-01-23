2d6cf309f13af44a41aa3e6d96ef61166992e384
korut94 <andreamnt94@gmail.com>
2015-01-23 11:11:06 +0100

    Aggiunti vari costruttori

--- LinQedIn/marklineeditvalidate.h
+++ LinQedIn/marklineeditvalidate.h
@@ -1,10 +1,24 @@
 #ifndef MARKLINEEDITVALIDATE_H
 #define MARKLINEEDITVALIDATE_H
 
-class MarkLineEditValidate
+#include <QLabel>
+#include <QLineEdit>
+#include <QValidator>
+
+#include "checkgroupinput.h"
+
+class MarkLineEditValidate : public QLabel,
+                             public QLineEdit,
+                             public QValidator
 {
-public:
-    MarkLineEditValidate();
+    public:
+        MarkLineEditValidate( const CheckGroupInput::CheckInput & );
+
+        MarkLineEditValidate( const QString &,
+                              const CheckGroupInput::CheckInput & );
+
+        MarkLineEditValidate( const QString &, const QString &,
+                              const CheckGroupInput::CheckInput & );
 };
 
 #endif // MARKLINEEDITVALIDATE_H
