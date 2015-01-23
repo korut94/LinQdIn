81e7f87ec7f56d6b59b5d7cf1907c5713e74a886
korut94 <andreamnt94@gmail.com>
2015-01-23 07:21:32 +0100

    Aggiunti segnali e slot per l'invio e la manipolazioni dei dati immessi

--- LinQedIn/usersearch.cpp
+++ LinQedIn/usersearch.cpp
@@ -1,11 +1,32 @@
 #include "usersearch.h"
 
+void UserSearch::recapInfo()
+{
+    Info info( editName->text(), "Mantovani", "", "" );
+    emit sendUnknownUser( info );
+}
+
+
 UserSearch::UserSearch( QWidget * parent ) : QWidget( parent )
 {
+    QLabel * lblName = new QLabel( tr( "Name" ) );
+    QLineEdit * editName = new QLineEdit();
+    QPushButton * btm = new QPushButton();
 
+    QHBoxLayout * layoutName = new QHBoxLayout;
+    layoutName->addWidget( lblName );
+    layoutName->addWidget( editName );
+    layoutName->addWidget( btm );
+
+    QVBoxLayout * layout = new QVBoxLayout;
+
+    setLayout( layout );
+
+    connect( btm, SIGNAL( clicked() ), this, SLOT( searchUser() ) );
 }
 
 
 UserSearch::~UserSearch()
 {
+    delete editName;
 }
--- LinQedIn/usersearch.h
+++ LinQedIn/usersearch.h
@@ -1,10 +1,24 @@
 #ifndef USERSEARCH_H
 #define USERSEARCH_H
 
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 #include <QWidget>
+
+#include "info.h"
 
 class UserSearch : public QWidget
 {
+    QLineEdit * editName;
+
+    signals:
+        void sendUnknownUser( const Info & );
+
+    private slots:
+        void recapInfo();
+
     public:
         UserSearch( QWidget * = nullptr );
         ~UserSearch();
