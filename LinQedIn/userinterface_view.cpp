32658c8e2fe8851f376bbb335d426118c7d22e27
korut94 <andreamnt94@gmail.com>
2015-01-25 14:03:07 +0100

    Primi test

--- LinQedIn/userinterface_view.cpp
+++ LinQedIn/userinterface_view.cpp
@@ -2,7 +2,11 @@
 
 UserInterface_View::UserInterface_View( QWidget * parent ) : QWidget( parent )
 {
+    QPushButton * button = new QPushButton( "Push" );
+    QVBoxLayout * layout = new QVBoxLayout;
+    layout->addWidget( button );
 
+    setLayout( layout );
 }
 
 
