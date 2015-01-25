a9e8858686015bae749c83c72069432efd367740
korut94 <andreamnt94@gmail.com>
2015-01-25 12:27:43 +0100

    Aggiunti controller Admin e User

--- LinQedIn/mainwindow_model.h
+++ LinQedIn/mainwindow_model.h
@@ -1,8 +1,15 @@
 #ifndef MAINWINDOW_MODEL_H
 #define MAINWINDOW_MODEL_H
 
+#include "admininterface_controller.h"
+#include "userinterface_controller.h"
+
 class MainWindow_Model
 {
+    private:
+        AdminInterface_Controller * admin;
+        UserInterface_Controller * user;
+
     public:
         MainWindow_Model();
 };
