7a0ab6a0079dd1433f8a9e474da083249ca7c4d9
korut94 <andreamnt94@gmail.com>
2015-01-25 12:40:32 +0100

    Aggiunto costruttore

--- LinQedIn/mainwindow_model.cpp
+++ LinQedIn/mainwindow_model.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow_model.h"
 
 MainWindow_Model::MainWindow_Model()
+                  : admin( new AdminInterface_Controller ),
+                    user( new UserInterface_Controller )
 {
 }
