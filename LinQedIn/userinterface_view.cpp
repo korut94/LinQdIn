65a859d64f6d68a297ef69f38c6d2f14c590c070
korut94 <andreamnt94@gmail.com>
2015-01-27 11:35:57 +0100

    Abbozzo di interffaccia utente

--- LinQedIn/userinterface_view.cpp
+++ LinQedIn/userinterface_view.cpp
@@ -1,12 +1,118 @@
 #include "userinterface_view.h"
 
+UserInterface_View::ID::ID( LevelAccess level, QWidget * parent )
+                            : QWidget( parent ),
+                              name( new QStackedWidget ),
+                              surname( new QStackedWidget ),
+                              telephone( new QStackedWidget ),
+                              data( new QStackedWidget ),
+                              job( new QStackedWidget )
+{
+    QPixmap image( "../LinQedIn/User_150x114.png" );
+
+    QLabel * lblImage = new QLabel();
+    lblImage->setPixmap( image );
+
+    name->addWidget( new QLabel( "Andrea" ) );
+    name->addWidget( new QLineEdit( "Andrea" ) );
+    surname->addWidget( new QLabel( "Mantovani" ) );
+    surname->addWidget( new QLineEdit( "Mantovani" ) );
+    telephone->addWidget( new QLabel( "3406936174" ) );
+    telephone->addWidget( new QLineEdit( "3406936174" ) );
+    data->addWidget( new QLabel( "17/09/1994" ) );
+    data->addWidget( new QLineEdit( "17/09/1994" ) );
+    job->addWidget( new QLabel( "Student" ) );
+    job->addWidget( new QLineEdit( "Student" ) );
+
+    QHBoxLayout * layoutFullName = new QHBoxLayout;
+    layoutFullName->addWidget( name );
+    layoutFullName->addWidget( surname );
+
+    telephone->setCurrentIndex( 1 );
+
+    QFormLayout * layoutInfo = new QFormLayout;
+    layoutInfo->addRow( tr( "Telephone" ) + ':', telephone );
+    layoutInfo->addRow( tr( "Data" ) + ':', data );
+    layoutInfo->addRow( tr( "Job" ) + ':', job );
+
+    QVBoxLayout * layoutDati = new QVBoxLayout;
+    layoutDati->addLayout( layoutFullName );
+    layoutDati->addLayout( layoutInfo );
+
+    QHBoxLayout * layout = new QHBoxLayout;
+    layout->addWidget( lblImage );
+    layout->addLayout( layoutDati );
+
+    setLayout( layout );
+}
+
+
+UserInterface_View::ID::~ID()
+{
+    delete name;
+    delete surname;
+    delete telephone;
+    delete data;
+    delete job;
+}
+
+
+UserInterface_View::Top::Top( LevelAccess level, QWidget * parent )
+                              : QWidget( parent ),
+                                username( new QStackedWidget ),
+                                typeAccount( new QStackedWidget )
+
+{
+    QLabel * lblUser = new QLabel( tr( "Username" ) + ':' );
+    username->addWidget( new QLabel( "Username" ) );
+    username->addWidget( new QLineEdit( "Username" ) );
+
+    QLabel * lblAccount = new QLabel( tr( "Account" ) + ':' );
+    typeAccount->addWidget( new QLabel( "Type account" ) );
+    typeAccount->addWidget( new QLabel( "Type account" ) );
+
+    QPushButton * btmFriend = new QPushButton( tr( "Add coworker" ) );
+
+    QHBoxLayout * layout = new QHBoxLayout;
+    layout->addWidget( lblUser );
+    layout->addWidget( username );
+    layout->addWidget( lblAccount );
+    layout->addWidget( typeAccount );
+    layout->addWidget( btmFriend );
+
+    setLayout( layout );
+}
+
+
+UserInterface_View::Top::~Top()
+{
+    delete username;
+    delete typeAccount;
+}
+
+
 UserInterface_View::UserInterface_View( LevelAccess l, QWidget * parent )
                                         : level( l ),
                                           QWidget( parent )
 {
-    QPushButton * button = new QPushButton( "Push" );
+    QScrollArea * area = new QScrollArea;
+
+    Top * top = new Top( l );
+    ID * id = new ID( l );
+
+    QVBoxLayout * layoutUserData = new QVBoxLayout;
+    layoutUserData->setAlignment( Qt::AlignTop );
+    layoutUserData->addWidget( top );
+    layoutUserData->addWidget( id );
+
+    QHBoxLayout * layoutInterface = new QHBoxLayout;
+    layoutInterface->addLayout( layoutUserData );
+
+    area->setLayout( layoutInterface );
+
     QVBoxLayout * layout = new QVBoxLayout;
-    layout->addWidget( button );
+    area->setWidgetResizable( true );
+    layout->addWidget( area );
 
     setLayout( layout );
 }
--- LinQedIn/userinterface_view.h
+++ LinQedIn/userinterface_view.h
@@ -1,7 +1,13 @@
 #ifndef USERINTERFACE_VIEW_H
 #define USERINTERFACE_VIEW_H
 
+#include <iostream>
+#include <QFormLayout>
+#include <QLabel>
+#include <QLineEdit>
 #include <QPushButton>
+#include <QStackedWidget>
+#include <QScrollArea>
 #include <QVBoxLayout>
 #include <QWidget>
 
@@ -11,11 +17,46 @@ enum LevelAccess
     Basic,
     Business,
     Executive,
+    Modify,
     Master
 };
 
 class UserInterface_View : public QWidget
 {
+    private:
+        class ViewExperience : public QWidget
+        {
+            private:
+
+            public:
+
+        };
+
+        class ID: public QWidget
+        {
+            private:
+                QStackedWidget * name;
+                QStackedWidget * surname;
+                QStackedWidget * telephone;
+                QStackedWidget * data;
+                QStackedWidget * job;
+
+            public:
+                ID( LevelAccess, QWidget * = nullptr );
+                ~ID();
+        };
+
+        class Top : public QWidget
+        {
+            private:
+                QStackedWidget * username;
+                QStackedWidget * typeAccount;
+
+            public:
+                Top( LevelAccess, QWidget * = nullptr );
+                ~Top();
+        };
+
     private:
         LevelAccess level;
 
