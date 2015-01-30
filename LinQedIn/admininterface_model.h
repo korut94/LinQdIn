a7e08faddb48183f51959aaf905c8f94579a21c8
korut94 <andreamnt94@gmail.com>
2015-01-30 18:39:08 +0100

    Aggiunto punatatore a Database

--- LinQedIn/admininterface_model.h
+++ LinQedIn/admininterface_model.h
@@ -1,8 +1,13 @@
 #ifndef ADMININTERFACE_MODEL_H
 #define ADMININTERFACE_MODEL_H
 
+#include "database.h"
+
 class AdminInterface_Model
 {
+    private:
+        Database * database;
+
     public:
         AdminInterface_Model();
 };
