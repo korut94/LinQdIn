989478c3bc9f161a80dbc3160b167ee771733793
korut94 <andreamnt94@gmail.com>
2015-02-01 18:17:44 +0100

    Aggiunto controller UserSearch

--- LinQedIn/usersearch.cpp
+++ LinQedIn/usersearch.cpp
@@ -1,32 +1,101 @@
 #include "usersearch.h"
 
-UserSearch::UserSearch( QWidget * parent ) : QWidget( parent )
+void UserSearch::manageLocalError( ErrorState::Type type )
 {
+    switch( type )
+    {
+        case ErrorState::InvalidValue :
+             emit errorMessage( tr( "Incorrect values" ) );
+             break;
+
+        case ErrorState::EmptyValue :
+             emit errorMessage( tr( "Empty values" ) );
+             break;
+    }
+}
+
+
+void UserSearch::checkToSanityInsert() const
+{
+    if( editPage->checkErrorForm() == ErrorState::None )
+    {
+        if( editPage->completeForm() ) emit search( editPage->recapInfo() );
+        else emit error( ErrorState::EmptyValue );
+    }
+
+    else emit error( ErrorState::InvalidValue );
+}
+
+
+UserSearch::UserSearch( LevelAccess::Type l, QWidget * parent )
+                        : QWidget( parent ), level( l )
+{
+
+}
+
+
+UserSearch::~UserSearch()
+{
+}
+
+
+QWidget * UserSearch::getView() const
+{
+    QWidget * view = new QWidget();
+
     QPushButton * btnReset = new QPushButton( tr( "Reset" ) );
     QPushButton * btnSearch = new QPushButton( tr( "Search" ) );
 
-    UserEditPage * editPage = new UserEditPage();
+    editPage->loadModuleId();
+    editPage->loadModuleExperience();
+    editPage->loadModuleSkill();
+    editPage->loadModuleEducation();
 
     QHBoxLayout * layoutButton = new QHBoxLayout;
     layoutButton->setAlignment( Qt::AlignRight );
     layoutButton->addWidget( btnSearch );
     layoutButton->addWidget( btnReset );
 
-
     QVBoxLayout * layoutBottom = new QVBoxLayout;
     layoutBottom->setAlignment( Qt::AlignBottom );
     layoutBottom->addWidget( new Line() );
     layoutBottom->addLayout( layoutButton );
 
-
     QVBoxLayout * layout = new QVBoxLayout;
     layout->addWidget( editPage );
     layout->addLayout( layoutBottom );
 
-    setLayout( layout );
-}
+    view->setLayout( layout );
 
+    connect( editPage,
+             SIGNAL( requestAddEducation() ),
+             editPage,
+             SLOT( addEducation() ) );
 
-UserSearch::~UserSearch()
-{
+    connect( editPage,
+             SIGNAL( requestAddExperience() ),
+             editPage,
+             SLOT( addExperience() ) );
+
+    connect( editPage,
+             SIGNAL( requestAddSkill() ),
+             editPage,
+             SLOT( addSkill() ) );
+
+    connect( btnReset,
+             SIGNAL( clicked() ),
+             editPage,
+             SLOT( reset() ) );
+
+    connect( btnSearch,
+             SIGNAL( clicked() ),
+             this,
+             SLOT( checkToSanityInsert() ) );
+
+    connect( this,
+             SIGNAL( errorMessage( const QString & ) ),
+             editPage,
+             SLOT( error( const QString &) ) );
+
+    return view;
 }
--- LinQedIn/usersearch.h
+++ LinQedIn/usersearch.h
@@ -5,7 +5,9 @@
 #include <QPushButton>
 #include <QWidget>
 
+#include "errorstate.h"
 #include "info.h"
+#include "levelaccess.h"
 #include "line.h"
 #include "usereditpage.h"
 
@@ -13,12 +15,24 @@ class UserSearch : public QWidget
 {
     Q_OBJECT
 
+    private:
+        LevelAccess::Type level;
+        UserEditPage * editPage;
+
     signals:
-        void searchUser( const Info & );
+        void error( ErrorState::Type ) const;
+        void errorMessage( const QString & ) const;
+        void search( const Info & ) const;
+
+    public slots:
+        void checkToSanityInsert() const;
+        void manageLocalError( ErrorState::Type );
 
     public:
-        UserSearch( QWidget * = nullptr );
+        UserSearch( LevelAccess::Type, QWidget * = nullptr );
         ~UserSearch();
+
+        QWidget * getView() const;
 };
 
 #endif // USERSEARCH_H
