25d0643429ff263e9e1167ac4d82511d27a6e7a0
korut94 <andreamnt94@gmail.com>
2015-02-11 15:36:14 +0100

    Aggiunta definizione metodo virtuale load

--- LinQedIn/databasexmlfile.cpp
+++ LinQedIn/databasexmlfile.cpp
@@ -10,13 +10,64 @@ DatabaseXmlFile::~DatabaseXmlFile()
 
 void DatabaseXmlFile::load()
 {
+	setFlagLoad( false );
 
+	open( QIODevice::ReadOnly );
+
+	/*
+	 * L'uso del vector serve nel caso ci sia un errore nel parsing 
+	 * durante il caricamento
+	*/
+	QVector<smartptr_utente> usersLoadFile;
+
+	QXmlStreamReader reader( this );
+
+	while( !reader.atEnd() && !reader.hasError() )
+	{
+		QXmlStreamReader::TokenType token = reader.readNext();
+
+		if( token == QXmlStreamReader::StartElement )
+		{
+			if( reader.name() == "Utente" )
+			{
+				smartptr_utente user;
+
+				if( reader.attributes().value( "type" ) == "Basic" )
+					user = new UtenteBasic();
+
+				else if( reader.attributes().value( "type" ) == "Business" )
+					user = new UtenteBusiness();
+
+				else if( reader.attributes().value( "type" ) == "Executive" )
+					user = new UtenteExecutive();
+	
+				user->readXmlFormat( reader );
+				usersLoadFile.push_back( user );
+			}
+		}
+	}
+
+	if( reader.hasError() ) std::cout << reader.errorString().toStdString() << std::endl;
+	else
+	{
+		for( QVector<smartptr_utente>::const_iterator 
+									   itr = usersLoadFile.begin();	
+			 itr != usersLoadFile.end();
+			 itr++ )
+		{
+			insert( *itr );
+		}
+		
+		setFlagLoad( true );
+	}	
+
+	close();
 }
 
 
 void DatabaseXmlFile::save()
 {
-	if( !isModified() )
+	if( isModified() )
 	{ 
     	if( !isLoaded() ) QFile::remove( fileName() ); //sovrascrittura
 
@@ -26,7 +77,6 @@ void DatabaseXmlFile::save()
     	writer.setAutoFormatting( true );
 
     	writer.writeStartDocument();
-
     	writer.writeStartElement( "Database" );
 
     	QVector<smartptr_utente> risp = getUsers( SearchGroupUtente::All() );
@@ -39,7 +89,6 @@ void DatabaseXmlFile::save()
     	}
 
     	writer.writeEndElement();
-
     	writer.writeEndDocument();
 
 		setFlagModify( false );
