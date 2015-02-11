4c75826635e3f7044d524ecf17a0a0062e00d47f
korut94 <andreamnt94@gmail.com>
2015-02-11 11:11:25 +0100

    Gestita la modifica a questo livello

---
+++ database.h
@@ -0,0 +1,55 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <iostream>
+#include <QList>
+
+#include "frankenstein.h"
+#include "hashgrouputente.h"
+#include "hashlistutente.h"
+#include "query.h"
+#include "searchgrouputente.h"
+#include "utente.h"
+
+class Database
+{
+    private:
+        enum Category
+        {
+            Name,
+            Surname,
+            Username,
+            General
+        };
+
+		bool modified;
+
+        HashListUtente<HashGroupUtente::NameCrescente> entryName;
+        HashListUtente<HashGroupUtente::CognomeCrescente> entrySurname;
+        HashListUtente<HashGroupUtente::UsernameCrescente> entryUsername;
+
+        QList<smartptr_utente> general;
+
+        Category getCategory( const Frankenstein & ) const;
+
+	protected:
+		void setFlagModify( bool );
+
+    public:
+        Database();
+
+        bool isPresent( const smartptr_utente & ) const;
+		bool isModified() const;
+
+        QVector<smartptr_utente> getUsers( const Query & ) const;
+
+        smartptr_utente recoveryUser( const smartptr_utente & ) const;
+
+        void insert( const smartptr_utente & );
+        void linkUser( const smartptr_utente &, const smartptr_utente & );
+        void modify( const smartptr_utente &, const Info & );
+        void remove( const smartptr_utente & );
+        void unlinkUser( const smartptr_utente &, const smartptr_utente & );
+};
+
+#endif // DATABASE_H
